26c90509d30d5fed64896927d5b07b862962d515
Florian Schießl <florian@floriware.de>
2013-07-03 12:40:26 +0200

    Deleted old Source Folder

--- de/floriware/gui/chatsimple/IConnectWindowHandler.java
+++
@@ -1,9 +0,0 @@
-package de.floriware.gui.chatsimple;
-
-import de.floriware.chatsimple.ServerInfo;
-
-public interface IConnectWindowHandler
-{
-	public void e_connect(ServerInfo sinfo);
-	public void e_abort();
-}
